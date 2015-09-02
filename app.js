e9b078813acea2c0123057ab1a2f2bbece277616
Loren Barrick <lorenbarrick@gmail.com>
2015-09-02 15:12:01 -0700

    refactor to function

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var confirmation = {
-  init: function() {
-    $('.confirmation').on('click', 'button', this.loadConfirmation);
-    $('.confirmation').on('click', '.view-boarding-pass', this.showBoardingPass );
-  },
-  loadConfirmation: function() {
+function Confirmation(el)  {
+  this.el = el;
+  this.ticket = this.el.find('.ticket');
+  var confirmation = this;
+  this.loadConfirmation = function() {
     $.ajax('confirmation.html', {
       success: function(response) {
-        $('.ticket').html(response).slideDown();
+        this.ticket.html(response).slideDown();
       },
       error: function(request, errorType, errorMessage) {
         alert('Error: ' + errorType + ' with message: ' + errorMessage);
       },
       timeout: 3000,
+      context: confirmation,
       beforeSend: function() {
         $('.confirmation').addClass('is-loading');
       },
@@ -21,19 +21,29 @@ var confirmation = {
       data: { "confNum": 1234}
     });
   },
-  showBoardingPass: function(event) {
+  this.showBoardingPass = function(event) {
+    event.preventDefault();
     //listens for click events inside .confirmation when they happen check if the target was .view-boarding-pass
-    $('.confirmation').on('click', '.view-boarding-pass', function() {
-      $(this).closest('.ticket').find('img').show();
-    });
+    $(this).hide();
+    confirmation.el.find('.boarding-pass').show();
+    // $('.confirmation').on('click', '.view-boarding-pass', function() {
+    //   $(this).closest('.ticket').find('img').show();
+    // });
   }
-};
+  this.el.on('click', 'button', this.loadConfirmation);
+  this.el.on('click', '.view-boarding-pass', this.showBoardingPass);
+}
 
 $(document).ready(function() {
+  var paris = new Confirmation($('#paris'));
+  var london = new Confirmation($('#london'));
   confirmation.init();
 }
 
-
+// init: function() {
+//   $('.confirmation').on('click', 'button', this.loadConfirmation);
+//   $('.confirmation').on('click', '.view-boarding-pass', this.showBoardingPass );
+// },
 
   // $('.confirmation').on('click', 'button', function() {
   //   $(this).find('.ticket').slideDown();
