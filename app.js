7b839a1f533472f0e2d71b5e39c71dc8ca629ec3
Loren Barrick <lorenbarrick@gmail.com>
2015-09-02 14:29:32 -0700

    clean code by using objects

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-$(document).ready(function() {
-  $('.confirmation').on('click', 'button', function() {
-    $(this).find('.ticket').slideDown();
-  });
-  //listens for click events inside .confirmation when they happen check if the target was .view-boarding-pass
-  $('.confirmation').on('click', '.view-boarding-pass', function() {
-    $(this).closest('.ticket').find('img').show();
-  });
-  $('.confirmation').on('click', 'button', function() {
+var confirmation = {
+  init: function() {
+    $('.confirmation').on('click', 'button', this.loadConfirmation);
+    $('.confirmation').on('click', '.view-boarding-pass', this.showBoardingPass );
+  },
+  loadConfirmation: function() {
     $.ajax('confirmation.html', {
       success: function(response) {
         $('.ticket').html(response).slideDown();
@@ -23,7 +20,26 @@ $(document).ready(function() {
       },
       data: { "confNum": 1234}
     });
-  });
+  },
+  showBoardingPass: function(event) {
+    //listens for click events inside .confirmation when they happen check if the target was .view-boarding-pass
+    $('.confirmation').on('click', '.view-boarding-pass', function() {
+      $(this).closest('.ticket').find('img').show();
+    });
+  }
+};
+
+$(document).ready(function() {
+  confirmation.init();
+}
+
+
+
+  // $('.confirmation').on('click', 'button', function() {
+  //   $(this).find('.ticket').slideDown();
+  // });
+
+
   //shorthand ajax syntax with get
   //$.get('confirmation.html', function(response) {
   //   $('.ticket').html(response).slideDown();
